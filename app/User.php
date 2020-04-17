e2363e692503ab40d956ca84cc482d071f3fc5b2
SardorAlijanov <s.alijanov@studentinha.uz>
2020-04-17 22:19:08 +0500

    Added Eloquent model

--- app/User.php
+++ app/User.php
@@ -6,34 +6,19 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Model implements AuthenticatableContract,
+                                    AuthorizableContract,
+                                    CanResetPasswordContract
 {
-    use Notifiable;
+    use Authenticatable, Authorizable, CanResetPassword;
+
+    // Other Eloquent Properties...
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
+     * Get all of the tasks for the user.
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
